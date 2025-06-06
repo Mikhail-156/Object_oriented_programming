553d6cf9806dca58a418d41f439914c88c585d42
Михаил Ергунов <ergunov.mih@mail.ru>
2025-06-06 12:00:14 +0500

    writing tests

--- tests/test_products.py
+++ tests/test_products.py
@@ -7,3 +7,11 @@ def test_prod(fixture_product: Product) -> None:
     assert fixture_product.description == "256GB, Серый цвет, 200MP камера"
     assert fixture_product.price == 180000.0
     assert fixture_product.quantity == 5
+
+
+def test_prod_1(fixture_product_1: Product) -> None:
+    """Тест"""
+    assert fixture_product_1.name == "Xiaomi Redmi Note 11"
+    assert fixture_product_1.description == "512GB, Черный цвет, 200MP камера"
+    assert fixture_product_1.price == 0.0
+    assert fixture_product_1.quantity == 5
